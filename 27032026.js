342d11db76187ee7065d142664bdc3c23aecd50c
bonatokss <joao.bonato.santos@escola.pr.gov.br>
2026-03-27 07:56:28 -0300

    update

--- 27032026.js
+++ 27032026.js
@@ -2,7 +2,14 @@
 
 const fila = [1,2,3,4,5,6,];
 
-const filaStringAmigos = ["Renan Cabeçudo, Luiz Bigode, Luis Beiçudo, Pedro tcham, Rafa du Graul"]
+const filaStringAmigos = ["Renan Cabeçudo, Luiz Bigode, Luis Beiçudo, Pedro tcham, Rafa du Graul, Bob Zovudo"]
 
+function lerAmigos(filaStringAmigos){
+    console.log(`Voce tem ${filaStringAmigos.lenght} amigos`)
+    for(let i = 0; i < filaStringAmigos.length;i++){
+     console.log(filaStringAmigos[i])
+     if(filaStringAmigos[i] === "Bob Zovudo"){
+        console.log(`Index do seu melhor amigo`)
 
-
+     }
+}    
